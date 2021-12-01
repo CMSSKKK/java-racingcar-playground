06dc486178adbf446290a670a6ead33442d83bcb
Minseok-Choi <cms05041@gmail.com>
2021-12-02 01:18:33 +0900

    feat : Car 객체 구현 및 랜덤 숫자에 따라서 움직이는 기능 구현

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,53 @@
+package domain;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Car {
+    private static final int STOP = 3;
+    private Name name;
+    private Position position;
+
+    public Car(String name) {
+       this(name,0);
+    }
+
+    public Car(String name, int position) {
+        this.name = new Name(name);
+        this.position = new Position(position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(name, car.name) && Objects.equals(position, car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
+
+    public int position() {
+        return position.status();
+    }
+
+    public void move(int randomNo) {
+        if(randomNo > STOP) {
+            position.plusPosition();
+        }
+    }
+
+    public void moveRandom() {
+        if(getRandomNo() > STOP) {
+            position.plusPosition();
+        }
+    }
+
+    public static int getRandomNo() {
+        Random random = new Random();
+        return random.nextInt(9);
+    }
+}
---
+++ src/test/java/domaintest/CarTest.java
@@ -0,0 +1,48 @@
+package domaintest;
+
+import domain.Car;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @Test
+    void create() {
+        Car pobi = new Car("pobi",0);
+        assertThat(pobi).isEqualTo(new Car("pobi"));
+    }
+
+    @Test
+    @DisplayName("자동차의 포지션을 확인하는 기능 체크")
+    void car_포지션_체크() {
+        Car pobi = new Car("pobi",0);
+        Car crong = new Car("crong",2);
+        assertThat(pobi.position()).isEqualTo(0);
+        assertThat(crong.position()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("자동차 move()시 위치가 한 칸씩 이동한다.")
+    void car_move() {
+        Car pobi = new Car("pobi",0);
+        Car crong = new Car("crong",2);
+        pobi.move(5);
+        crong.move(5);
+        assertThat(pobi.position()).isEqualTo(1);
+        assertThat(crong.position()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("자동차는 랜덤 숫자 중 3이하의 수에는 움직이지 않고, 4이상의 수에는 움직인다.")
+    void car_moveR() {
+        Car pobi = new Car("pobi",0);
+        Car crong = new Car("crong",2);
+        pobi.move(3);
+        crong.move(4);
+        assertThat(pobi.position()).isEqualTo(0);
+        assertThat(crong.position()).isEqualTo(3);
+    }
+}
